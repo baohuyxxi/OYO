917f689002a68286e672fce29eba2011b601d8e5
letdtcode <boynguyenducthanh@gmail.com>
2023-10-23 16:48:17 +0700

    add api province

--- src/services/API/authAPI.js
+++ src/services/API/authAPI.js
@@ -1,45 +1,43 @@
-
-import axios from '../axios'
+import axios from "../axios";
 
 import {
-    SigninRequest,
-    CheckAccount,
-    RegisterRequest,
-    TokenRefreshRequest
-} from '~/share/model/auth'
+  SigninRequest,
+  CheckAccount,
+  RegisterRequest,
+  TokenRefreshRequest,
+} from "~/share/model/auth";
 
 export const loginRequest = async (LoginRequest) => {
-    try {
-        const res = await axios.post("v1/auth/signin", LoginRequest)
-        return res
-    } catch (error) {
-        return error.response
-    }
-}
+  try {
+    const res = await axios.post("/auth/signin", LoginRequest);
+    return res;
+  } catch (error) {
+    return error.response;
+  }
+};
 export const checkAccount = async (CheckAccount) => {
-    try {
-        const res = await axios.post("v1/public/user/checkmail", CheckAccount)
-        return res
-    } catch (error) {
-        return error.response
-    }
-}
+  try {
+    const res = await axios.post("/public/user/checkmail", CheckAccount);
+    return res;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-export const  registerRequest= async (RegisterRequest) => {
-    try {
-        const res = await axios.post("v1/auth/signup", RegisterRequest)
-        return res
-    } catch (error) {
-        return error.response
-    }
-}
+export const registerRequest = async (RegisterRequest) => {
+  try {
+    const res = await axios.post("/auth/signup", RegisterRequest);
+    return res;
+  } catch (error) {
+    return error.response;
+  }
+};
 
-export const  tokenRefreshRequest= async (TokenRefreshRequest) => {
-    try {
-        const res = await axios.post("user/signin", TokenRefreshRequest)
-        return res
-    } catch (error) {
-        return error.response
-    }
-}
-
+export const tokenRefreshRequest = async (TokenRefreshRequest) => {
+  try {
+    const res = await axios.post("user/signin", TokenRefreshRequest);
+    return res;
+  } catch (error) {
+    return error.response;
+  }
+};

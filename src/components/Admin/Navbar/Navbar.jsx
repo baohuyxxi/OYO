be5a776d352ca447c39f84788ead49abf79f5821
letdtcode <boynguyenducthanh@gmail.com>
2023-11-26 22:42:18 +0700

    update admin

---
+++ src/components/Admin/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// const Navbar = () => {
+//   return (
+//     <div className="topnav-admin">
+//       <div style={{ marginTop: "20px" }}>{/* <FilterAdmin /> */}</div>
+
+//       <div className="topnav__right">
+//         <div className="topnav__right-item">
+//           {/* dropdown here */}
+//           <Dropdown
+//             customToggle={() => renderUserToggle(curr_user)}
+//             contentData={user_menu}
+//             renderItems={(item: any, index: any) => renderUserMenu(item, index)}
+//           />
+//         </div>
+//         <div className="topnav__right-item">
+//           <ThemeMenu />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;

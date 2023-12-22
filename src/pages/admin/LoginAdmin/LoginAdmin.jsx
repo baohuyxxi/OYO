59dfb29ccb49f5137a42655ea0b1d59be56a4df3
letdtcode <boynguyenducthanh@gmail.com>
2023-12-22 17:43:46 +0700

    update

---
+++ src/pages/admin/LoginAdmin/LoginAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginAdmin = () => {
+    return (
+        <div className="login__admin">
+            <h1>Xin </h1>
+        </div>
+    );
+};
+
+export default LoginAdmin;

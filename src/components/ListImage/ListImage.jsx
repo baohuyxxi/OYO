8ccf6a5bcb56c55efbc82088ca8a8cfbd6f82d20
letdtcode <boynguyenducthanh@gmail.com>
2024-02-21 21:23:36 +0700

    add video in detail

--- src/components/ListImage/ListImage.jsx
+++ src/components/ListImage/ListImage.jsx
@@ -1,20 +1,34 @@
 import './ListImage.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import globalSlice from '~/redux/globalSlice';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 
+import { Cloudinary } from '@cloudinary/url-gen';
+import { AdvancedVideo } from '@cloudinary/react';
+import { scale } from '@cloudinary/url-gen/actions/resize';
+
 const ListImage = (props) => {
-    const dispatch = useDispatch();
     const imgMain = props.listImage.slice(0, 1);
     const cutDataImage = props.listImage.slice(1, 5);
     const remainingIamge = props.listImage.length;
+
+    const cld = new Cloudinary({
+        cloud: {
+            cloudName: 'dyv5zrsgj'
+        }
+    });
+
+    const crop = cld
+        .video('oyo_booking/video/d6rue37bifn53ztpo2u4')
+        .resize(scale().width(810).height(500).aspectRatio('16:9'))
+        .quality('auto')
+        .format('auto');
+
     return (
         <div className="list-image">
             {props.listImage ? (
                 <div className="row">
                     <div className="col l-6 c-12">
                         <div className="image-item-thumbnail">
-                            <img src={imgMain} alt="room_hot" onClick={() => props.setOpen(true)} />
+                            <AdvancedVideo autoPlay controls loop cldVid={crop} />
                         </div>
                     </div>
                     <div className="col l-6 c-12">
@@ -22,10 +36,17 @@ const ListImage = (props) => {
                             {cutDataImage.map((imgs, index) => (
                                 <div className="col l-6 c-6" key={index}>
                                     <div className="image-item">
-                                        <img src={imgs} alt="room_hot" onClick={() => props.setOpen(true)} className={`img__${index}`} />
+                                        <img
+                                            src={imgs}
+                                            alt="room_hot"
+                                            onClick={() => props.setOpen(true)}
+                                            className={`img__${index}`}
+                                        />
                                         {remainingIamge > 4 && index == 3 && (
                                             <div className="overlay" onClick={() => props.setOpen(true)}>
-                                                <div className="overlay-text">+{remainingIamge - 4} <ImageOutlinedIcon/></div>
+                                                <div className="overlay-text">
+                                                    +{remainingIamge - 4} <ImageOutlinedIcon />
+                                                </div>
                                             </div>
                                         )}
                                     </div>

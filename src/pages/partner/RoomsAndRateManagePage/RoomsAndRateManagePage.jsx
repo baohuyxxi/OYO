9397b3db17e7d7529598180721e1cc11f31971b7
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2024-07-03 16:16:33 +0700

    Fix color correction and price correction errors

--- src/pages/partner/RoomsAndRateManagePage/RoomsAndRateManagePage.jsx
+++ src/pages/partner/RoomsAndRateManagePage/RoomsAndRateManagePage.jsx
@@ -5,11 +5,29 @@ import Skeleton from '@mui/material/Skeleton'; // Import Skeleton
 import { useEffect } from 'react';
 import { useFetchAccomData } from '~/redux/managerAccomSlice';
 import FramePage from '~/components/FramePage/FramePage';
+
 export default function RoomsAndRateManagePage() {
     useFetchAccomData();
     const { accomPriceCustom, accomApproved, accomWaiting, loading, error } = useSelector(
         (state) => state.managerAccom
     );
+
+    const renderSkeletonTable = () => {
+        return (
+            <div className="skeleton-table">
+                <div className="skeleton-table__body">
+                    {Array(5).fill().map((_, rowIndex) => (
+                        <div className="skeleton-table__row" key={rowIndex}>
+                            <Skeleton variant="rectangular" width="100%" height={50} />
+                            {Array(8).fill().map((_, colIndex) => (
+                                <Skeleton key={colIndex} variant="rectangular" width="100%" height={50} />
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    };
 
     return (
         <FramePage ownerPage={true}>
@@ -17,7 +35,7 @@ export default function RoomsAndRateManagePage() {
                 <div className="rooms-and-rate-manager__header">
                     <h1 className="page-header">Quản lý phòng và giá</h1>
                     {loading === 'loading' ? (
-                        <Skeleton variant="rectangular" width="100%" height={50} />
+                        renderSkeletonTable()
                     ) : (
                         <RoomsAndRate accomPriceCustom={accomPriceCustom} />
                     )}

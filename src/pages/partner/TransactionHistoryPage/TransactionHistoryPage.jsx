69768aaa946a95567e71a883ffbee9537898fe04
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2024-07-17 16:35:58 +0700

    add search home and css t7 cn

--- src/pages/partner/TransactionHistoryPage/TransactionHistoryPage.jsx
+++ src/pages/partner/TransactionHistoryPage/TransactionHistoryPage.jsx
@@ -6,8 +6,8 @@ import FramePage from '~/components/FramePage/FramePage';
 
 const TransactionHistoryPage = () => {
     const bannerData = {
-        title: 'Quản lý phòng và giá',
-        subtitle: 'Kiểm tra và quản lý thông tin phòng và giá cả một cách dễ dàng.',
+        title: 'Quản lý lịch sử giao dịch',
+        subtitle: 'Kiểm tra và quản lý thông tin lịch sử giao dịch một cách dễ dàng.',
         imgSrc: 'https://quickbooks.intuit.com/oidam/intuit/sbseg/en_us/Blog/Graphic/how-to-write-a-business-plan-header-image-us-en.png'  
     };
     return (
